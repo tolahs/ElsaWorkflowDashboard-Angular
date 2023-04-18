a20d86d148ed254e83dc1082ea14cd25b86a331e
tolahs <tolahs@gmail.com>
2023-04-18 09:48:52 +0300

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+
+import axios from 'axios';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpRequest } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ElsaWorkflowDashboard-Angularv12';
+
+  isAuthenticated = false;
+  constructor(
+    public oidcSecurityService: OidcSecurityService,
+    private route: ActivatedRoute,
+    private authPl: Router
+  ) {}
+  ngOnInit() {
+    //   const elsaStudioRoot = new  ElsaStudioRoot();
+    //   elsaStudioRoot.addEventListener('initializing', e => {
+    //     const elsaStudio = e;
+    //     // elsaStudio.pluginManager.registerPlugin(this.authPl);
+    // });
+
+    // axios.interceptors.request.use(
+    //   (config) => {
+    //     config.headers['Authorization'] = `Bearer ${localStorage.getItem(
+    //       'access_token'
+    //     )}`;
+    //     return config;
+    //   },
+    //   (error) => {
+    //     return Promise.reject(error);
+    //   }
+    // );
+
+    this.route.fragment.subscribe((fragment) => {
+      const myParamValue = fragment;
+      if (fragment) {
+        const token = fragment.split('&')[0];
+        if (token.startsWith('access_token=')) {
+          localStorage.setItem(
+            'access_token',
+            token.replace('access_token=', '')
+          );
+        }
+      }
+    });
+    this.oidcSecurityService.isAuthenticated$.subscribe(
+      ({ isAuthenticated }) => {
+        this.isAuthenticated = isAuthenticated;
+
+        // console.warn('authenticated: ', isAuthenticated);
+      }
+    );
+  }
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+  initializing(event: any) {
+    const elsaStudio = event.detail;
+    elsaStudio.pluginManager.registerPlugin(async (params: any) => {
+      params.eventBus.on('http-client-created', (e:any) => {
+        // Register Axios middleware.
+        debugger;
+        e.service.register({
+            onRequest(request:any) {
+                request.headers = { 'Authorization':  `Bearer ${localStorage.getItem('access_token')}` }
+                return request;
+            }
+        });
+    });
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AuthConfigModule } from './auth/auth-config.module'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthConfigModule,
+    HttpClientModule
   ],
-  providers: [],
+ 
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {  }
